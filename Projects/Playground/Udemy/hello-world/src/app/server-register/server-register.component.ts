ccf9baf27a6774d275df3ca9840c24ffdce668a1
Ranjit Mane <ranjitmane48@gmail.com>
2023-02-19 15:58:57 +0530

    [hello-world]: Access DOM using @ViewChhild and local reference variable

--- Projects/Playground/Udemy/hello-world/src/app/server-register/server-register.component.ts
+++ Projects/Playground/Udemy/hello-world/src/app/server-register/server-register.component.ts
@@ -1,26 +1,30 @@
-import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-register',
   templateUrl: './server-register.component.html',
   styleUrls: ['./server-register.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerRegisterComponent {
+  
   title = 'app-hello-world'; 
-  newServerContent = '';
+
+  @ViewChild('serverContentInput')
+  serverContentInput : ElementRef | undefined;
 
   @Output()
   serverCreated = new EventEmitter<{type: string, serverName: string, serverContent: string}>();
+  
   @Output()
   bluePrintCreated = new EventEmitter<{type: string, serverName: string, serverContent: string}>();
 
   onAddServer(nameInput: HTMLInputElement) : any {
-    console.log(nameInput.value);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       type: 'server', 
       serverName: nameInput.value, 
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput?.nativeElement.value
     });  
   }
 
@@ -28,7 +32,7 @@ export class ServerRegisterComponent {
     this.bluePrintCreated.emit({
       type: 'server', 
       serverName: nameInput.value, 
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput?.nativeElement.value
     });
   }
 }
