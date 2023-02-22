f127881e1ecd13dcbdeb148ef76bd48d53a5933e
Ranjit Mane <ranjitmane48@gmail.com>
2023-02-22 23:41:15 +0530

    [eShop]: Added basic project structure with home page

--- Projects/src/Dev/eShop/src/app/app.module.ts
+++ Projects/src/Dev/eShop/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './dashboard/products/products.component';
+import { ProductComponent } from './dashboard/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    DashboardComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
