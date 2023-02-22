463bb835046a28f6935b333ffe2dd4d54ca8aa72
Ranjit Mane <ranjitmane48@gmail.com>
2023-02-22 13:35:59 +0530

    [test-directive]: 1.change elemenent properties using HostBinding decorator

--- Projects/src/Udemy/test-directive/src/app/better-highlight/better-highlight.directive.ts
+++ Projects/src/Udemy/test-directive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,16 +7,21 @@ export class BetterHighlightDirective implements OnInit {
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
+  @HostBinding('style.backgroundColor')
+  backgroundColor: string | undefined;
+
 
   ngOnInit(): void {
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') onMouseOver(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = "blue";
   }
 
   @HostListener('mouseleave') onMouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = "transparent";
   }
 }
