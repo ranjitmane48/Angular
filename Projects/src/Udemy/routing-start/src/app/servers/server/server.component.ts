c97980420f1c908fbc3bdad1969a8bfbd519bbab
Ranjit Mane <ranjitmane48@gmail.com>
2023-04-04 09:50:20 +0530

    1.Added functionality to preserve query params using queryParamsHandling

--- Projects/src/Udemy/routing-start/src/app/servers/server/server.component.ts
+++ Projects/src/Udemy/routing-start/src/app/servers/server/server.component.ts
@@ -28,6 +28,9 @@ export class ServerComponent implements OnInit {
 
   onEdit() {
     console.log('onEdit is called!');
-    this.router.navigate(['edit'], { relativeTo: this.route });
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
   }
 }
