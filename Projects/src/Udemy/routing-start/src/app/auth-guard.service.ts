7f734a37de75e7fbb0edd22771c033c531d470d6
Ranjit Mane <ranjitmane48@gmail.com>
2023-04-07 09:52:04 +0530

    Protected child (nested) routes with CanActivateChild

--- Projects/src/Udemy/routing-start/src/app/auth-guard.service.ts
+++ Projects/src/Udemy/routing-start/src/app/auth-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
   UrlTree,
@@ -10,8 +11,18 @@ import { Observable } from 'rxjs-compat';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
 
   canActivate(
     _route: ActivatedRouteSnapshot,
