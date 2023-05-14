eaabbc13a75edd0eb9c15a9e830b97da5a9b5880
Ranjit Mane <ranjitmane48@gmail.com>
2023-05-14 08:48:53 +0530

    created test-observables project

---
+++ Projects/src/Practice/test-rxjs/APM/src/app/products/product-list.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+import { Observable, catchError, of, EMPTY } from 'rxjs';
+import { ProductCategory } from '../product-categories/product-category';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductListComponent implements OnInit {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories: ProductCategory[] = [];
+
+  products$: Observable<Product[]> | undefined;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts().pipe(
+      catchError((error) => {
+        this.errorMessage = error;
+        //return of([]);
+        return EMPTY;
+      })
+    );
+    // this.productService.getProducts().subscribe({
+    //   next: (products) => (this.products = products),
+    //   error: (err) => (this.errorMessage = err),
+    // });
+  }
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
